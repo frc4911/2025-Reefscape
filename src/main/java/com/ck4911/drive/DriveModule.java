568bebb5c152ce05e8aa19c7c3103e5268b666d0
Riley Brewer <riley.brewer@gmail.com>
2025-01-15 00:07:26 -0800

    Use Tuner X generated swerve instead of our old code

--- src/main/java/com/ck4911/drive/DriveModule.java
+++ src/main/java/com/ck4911/drive/DriveModule.java
@@ -7,100 +7,18 @@
 
 package com.ck4911.drive;
 
-import com.ck4911.Constants.Mode;
 import com.ck4911.vision.VisionConsumer;
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
-import dagger.multibindings.IntKey;
-import dagger.multibindings.IntoMap;
-import dagger.multibindings.Multibinds;
-import java.util.Map;
-import javax.inject.Provider;
 
 @Module
 public interface DriveModule {
 
   @Provides
-  public static GyroIO providesGyroIO(Mode mode, Provider<GyroIO> realProvider) {
-    switch (mode) {
-      case REAL:
-        // TODO: enable this when gyro is set up
-        // return realProvider.get();
-        return new GyroIO() {};
-      case SIM:
-      default:
-        return new GyroIO() {};
-    }
+  public static Drive providesDrive() {
+    return TunerConstants.createDrivetrain();
   }
-
-  // FL, FR, BL, BR
-  @Provides
-  @IntoMap
-  @IntKey(0) // FL
-  public static ModuleIO providesFrontLeftModule(Mode mode) {
-    switch (mode) {
-      case REAL:
-        // TODO: enable this when swerve is set up
-        // return new ModuleIOTalonFX(0);
-        return new ModuleIO() {};
-      case SIM:
-        return new ModuleIOSim();
-      default:
-        return new ModuleIO() {};
-    }
-  }
-
-  @Provides
-  @IntoMap
-  @IntKey(1) // FR
-  public static ModuleIO providesFrontRightModule(Mode mode) {
-    switch (mode) {
-      case REAL:
-        // TODO: enable this when swerve is set up
-        // return new ModuleIOTalonFX(1);
-        return new ModuleIO() {};
-      case SIM:
-        return new ModuleIOSim();
-      default:
-        return new ModuleIO() {};
-    }
-  }
-
-  @Provides
-  @IntoMap
-  @IntKey(2) // BL
-  public static ModuleIO providesBackLeftModule(Mode mode) {
-    switch (mode) {
-      case REAL:
-        // TODO: enable this when swerve is set up
-        // return new ModuleIOTalonFX(2);
-        return new ModuleIO() {};
-      case SIM:
-        return new ModuleIOSim();
-      default:
-        return new ModuleIO() {};
-    }
-  }
-
-  @Provides
-  @IntoMap
-  @IntKey(3) // BR
-  public static ModuleIO providesBackRightModule(Mode mode) {
-    switch (mode) {
-      case REAL:
-        // TODO: enable this when swerve is set up
-        // return new ModuleIOTalonFX(3);
-        return new ModuleIO() {};
-      case SIM:
-        return new ModuleIOSim();
-      default:
-        return new ModuleIO() {};
-    }
-  }
-
-  @Multibinds
-  public abstract Map<Integer, Module> providesModules();
 
   @Binds
   public VisionConsumer bindsVisionConsumer(Drive drive);
