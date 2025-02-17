270d117b13636ef333353cf6c6e67d64b71b0877
Riley Brewer <riley.brewer@gmail.com>
2025-02-17 11:28:14 -0800

    Add controller stuff from bunny bots

---
+++ src/main/java/com/ck4911/control/Controller.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2025 FRC 4911
+// https://github.com/frc4911
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
+package com.ck4911.control;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import javax.inject.Qualifier;
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Controller {
+
+  public enum Role {
+    DRIVER,
+    OPERATOR
+  }
+
+  Role value();
+}
