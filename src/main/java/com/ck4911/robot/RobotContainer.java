c54831ba3d79bde6fa307cf72fab0611cc3988bf
Riley Brewer <riley.brewer@gmail.com>
2025-02-28 15:32:22 -0800

    don't auto-home

--- src/main/java/com/ck4911/robot/RobotContainer.java
+++ src/main/java/com/ck4911/robot/RobotContainer.java
@@ -8,26 +8,17 @@
 package com.ck4911.robot;
 
 import com.ck4911.auto.AutoCommandHandler;
-import com.ck4911.commands.CyberCommands;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import javax.inject.Inject;
 
 public class RobotContainer {
 
   private final AutoCommandHandler autoCommandHandler;
-  private final CyberCommands cyberCommands;
-  private final CommandScheduler commandScheduler;
 
   @Inject
-  public RobotContainer(
-      AutoCommandHandler autoCommandHandler,
-      CyberCommands cyberCommands,
-      CommandScheduler commandScheduler) {
+  public RobotContainer(AutoCommandHandler autoCommandHandler) {
     this.autoCommandHandler = autoCommandHandler;
-    this.cyberCommands = cyberCommands;
-    this.commandScheduler = commandScheduler;
   }
 
   /** Updates dashboard data. */
@@ -38,12 +29,10 @@ public class RobotContainer {
   public void autonomousInit() {
     System.out.println("AutoOn");
     autoCommandHandler.startCurrentCommand();
-    commandScheduler.schedule(cyberCommands.home());
   }
 
   public void teleopInit() {
     System.out.println("Teleop");
     autoCommandHandler.stopCurrentCommand();
-    commandScheduler.schedule(cyberCommands.home());
   }
 }
