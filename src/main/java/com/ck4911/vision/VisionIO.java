bf1b9514282fa64b29f041c98341fa6d64c8d05c
CyberKnights <>
2025-03-09 18:25:06 -0700

    Revert "photon vision?"
    This reverts commit f86b38d9004d2829543929d0e9c1d7db5ec39bea.

--- src/main/java/com/ck4911/vision/VisionIO.java
+++ src/main/java/com/ck4911/vision/VisionIO.java
@@ -9,28 +9,9 @@ package com.ck4911.vision;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Transform3d;
-import edu.wpi.first.math.geometry.Translation3d;
-import java.util.Optional;
 import org.littletonrobotics.junction.AutoLog;
-import org.photonvision.EstimatedRobotPose;
-import org.photonvision.PhotonCamera;
-import org.photonvision.PhotonPoseEstimator;
-import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 
 public interface VisionIO {
-  public final PhotonCamera camera = new PhotonCamera("grant's_spy_camera");
-  public final Transform3d robotToCam =
-      new Transform3d(
-          new Translation3d(0.5, 0.0, 0.5),
-          new Rotation3d(
-              0, 0,
-              0)); // Cam mounted facing forward, half a meter forward of center, half a meter up
-  // from center.
-  public final PhotonPoseEstimator photonPoseEstimator =
-      new PhotonPoseEstimator(
-          VisionModule.providesFieldLayout(), PoseStrategy.CLOSEST_TO_REFERENCE_POSE, robotToCam);
 
   @AutoLog
   class VisionIOInputs {
@@ -39,11 +20,6 @@ public interface VisionIO {
     public double timestampSeconds;
     public int[] targetIds;
     public boolean poseUpdated;
-  }
-
-  public static Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedRobotPose) {
-    photonPoseEstimator.setReferencePose(prevEstimatedRobotPose);
-    return photonPoseEstimator.update(camera.getLatestResult());
   }
 
   public default void updateInputs(VisionIOInputs inputs) {}
